e7d9aa7d01a6b459654dc86132db515ba643b6e3
MuhammadOsama169 <m.osama169@gmail.com>
2023-04-28 22:07:23 +0300

    feat: update portfolio

--- src/scenes/MyProjects.jsx
+++ src/scenes/MyProjects.jsx
@@ -1,12 +1,16 @@
-import { LineGradient } from "../components/LineGradient";
-import { motion } from "framer-motion";
-import homerunProject from "../assets/homerun-website.png"
-import renewbeProject from "../assets/renewbe-project.png"
-import superkickProject from "../assets/superkick-branding.png"
-import superkickLogo from "../assets/logo-superkick.png"
-import homerunLogo from "../assets/logo-homerun.png"
-import renewbeLogo from "../assets/logo-renewbe.png"
-import githubLogo from "../assets/github-logo.png"
+import { LineGradient } from '../components/LineGradient';
+import { motion } from 'framer-motion';
+import homerunProject from '../assets/homerun-website.png';
+import renewbeProject from '../assets/renewbe-project.png';
+import superkickProject from '../assets/superkick-branding.png';
+import superkickLogo from '../assets/logo-superkick.png';
+import homerunLogo from '../assets/logo-homerun.png';
+import renewbeLogo from '../assets/logo-renewbe.png';
+import SeriesOnlineLogo from '../assets/Serieslogo.png';
+import ImginifyLogo from '..//assets/imginifyLogo.png';
+import githubLogo from '../assets/github-logo.png';
+import ImaginifyProject from '../assets/imaginify.png';
+import SeriesOnlineProject from '../assets/series.png';
 
 export const MyProjects = () => {
   return (
@@ -31,7 +35,6 @@ export const MyProjects = () => {
             <LineGradient width="w-2/3" />
           </div>
         </div>
-
       </motion.div>
 
       {/* PROJECTS */}
@@ -54,7 +57,7 @@ export const MyProjects = () => {
           >
             BEAUTIFUL USER INTERFACES
           </div>
-            
+
           <div
             className="flex justify-center text-center items-center p-10 dark:bg-gradient-mint bg-gradient-purple
               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold text-[#30475E]"
@@ -68,105 +71,287 @@ export const MyProjects = () => {
             DYANAMIC & STATIC
           </div>
         </motion.div>
-
       </div>
 
-
       <div className="flex flex-col gap-10 pt-20 ">
-          {/* PROJECT 1 */}
-          {/* <div className="flex flex-col md:flex-row gap-1 pb-5">
-            
-            <div className="md:basis-[40%] md:order-1"> 
-              <img  alt="SuperKickProject.png" src={superkickProject} />
+        {/* SeriesOnline */}
+        <div className="flex flex-col xs:flex-col md:flex-row gap-1 pb-5 mt-10">
+          <div className=" md:basis-[40%] md:order-2 lg:order-1">
+            <img alt="HomerunProject.png" src={SeriesOnlineProject} />
+          </div>
+
+          <div className="md:basis-[60%] px-10 md:mb-10">
+            <div className=" md:basis-[40%] md:order-1 mb-3">
+              <img
+                className="pt-10 md:pt-0 w-[200px]"
+                alt="SeriesOnlineLogo.png"
+                src={SeriesOnlineLogo}
+              />
             </div>
-
-            <div className="md:basis-[60%] px-10 ">
-
-            <div className=" md:basis-[40%] md:order-1 mb-3 "> 
-              <img className="pt-10 md:pt-0" alt="SuperKickLogo.png" src={superkickLogo} />
-            </div>
-              <p className="dark:text-[#BEE0D6] text-black text-xl "> SuperKick Branding designs clear brand messaging to help your business stand out. For a business to stand out today the brand has to be unique and impactful.</p> 
-          
-              <div className="flex flex-col xs:flex-row gap-2 pt-5">
-                <a  href="https://superkickbranding.netlify.app/" className="hover:opacity-[0.8] transition duration-500 hover:scale-[1.1]">
-                  <button className="p-3 w-full xs:p-3.5 dark:bg-[#00FFB9] bg-redhot dark:text-black text-white rounded-sm py-5 px-7 font-semibold
-              hover:bg-blue hover:text-white transition duration-500">Demo website</button> 
-                </a>
-                
-                <a target="_blank" rel="noreferrer" href="https://github.com/MuhammadOsama169/project-outlaw" className="hover:opacity-[0.8] transition duration-500 hover:scale-[1.1]">
-                  <button className="dark:bg-[#F3EFE0] bg-white w-full p-3 flex flex-row text-black hover:bg-blue  transition duration-500 text-center align-middle items-center justify-center">
-                    <img className="w-[30px] xs:w-[30px] xs:h-[27px] mr-2" alt="github.png" src={githubLogo}/>
-                    View Code
-                  </button> 
-                </a>
-
-               </div>
-              </div>
-            </div> */}
-          <div>
-            {/* SECOND PROJECT */}
+            <p className="dark:text-[#BEE0D6] text-black text-xl">
+              Series Online: Is a clone of Series Online, A website where you
+              can check trending movies and their details.{' '}
+            </p>
             <div className="flex flex-col xs:flex-row gap-2 pt-5">
-              <div className=" md:basis-[40%] md:order-2 lg:order-1"> 
-                <img alt="HomerunProject.png" src={homerunProject} />
-              </div>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://seriesonline.netlify.app/"
+                className="hover:opacity-[0.8] transition duration-500 hover:scale-[1.1]"
+              >
+                <button
+                  className="p-3 w-full xs:p-3.5 dark:bg-[#00FFB9] bg-redhot dark:text-black text-white  rounded-sm py-5 px-7 font-semibold
+                  hover:bg-blue hover:text-white transition duration-500"
+                >
+                  Demo website
+                </button>
+              </a>
 
-              <div className="md:basis-[60%] px-10"> 
-                <div className=" md:basis-[40%] md:order-1 mb-3"> 
-                  <img  className="pt-10 md:pt-0" alt="HomeRunLogo.png" src={homerunLogo} />
-                </div>
-                <p className="dark:text-[#BEE0D6] text-black text-xl">HomeRun: Is a E-commerce website with custom backend entries and secure payemnt linked with STRIPE payment method. </p>
-                <div className="flex flex-col xs:flex-row gap-2 pt-5">
-
-                <a  href="https://homerun-muhammad.netlify.app/" className="hover:opacity-[0.8] transition duration-500 hover:scale-[1.1]">
-                  <button className="p-3 w-full xs:p-3.5 dark:bg-[#00FFB9] bg-redhot dark:text-black text-white  rounded-sm py-5 px-7 font-semibold
-              hover:bg-blue hover:text-white transition duration-500">Demo website</button> 
-                </a>
-
-                <a target="_blank" rel="noreferrer" href="https://github.com/MuhammadOsama169/HomeRun" className="hover:opacity-[0.8] transition duration-500 hover:scale-[1.1]">
-                  <button className="dark:bg-[#F3EFE0]  text-black bg-white w-full p-3 flex flex-row  hover:bg-blue  transition duration-500 text-center align-middle items-center justify-center">
-                      <img className="w-[30px] xs:w-[30px] xs:h-[27px] mr-2" alt="github.png" src={githubLogo}/>
-                      View Code
-                    </button> 
-                </a>
-
-                </div>
-              </div>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/MuhammadOsama169/Series_Online"
+                className="hover:opacity-[0.8] transition duration-500 hover:scale-[1.1]"
+              >
+                <button className="dark:bg-[#F3EFE0]  text-black bg-white w-full p-3 flex flex-row  hover:bg-blue  transition duration-500 text-center align-middle items-center justify-center">
+                  <img
+                    className="w-[30px] xs:w-[25px] xs:h-[27px] mr-2"
+                    alt="github.png"
+                    src={githubLogo}
+                  />
+                  View Code
+                </button>
+              </a>
             </div>
           </div>
-          <div>
-            {/* THIRD PROJECT */}
-          <div className="flex flex-col xs:flex-row gap-2 pt-5">
-            <div className=" md:basis-[40%] md:order-2"> 
-              <img  alt="RenewbeProject.png" src={renewbeProject} />
+        </div>
+      </div>
+      <div>
+        {/* Open Ai */}
+        <div className="flex flex-col xs:flex-col md:flex-row gap-1 pb-5 mt-10">
+          <div className=" md:basis-[40%] md:order-2 lg:order-1">
+            <img alt="ImaginifyProject.png" src={ImaginifyProject} />
+          </div>
+
+          <div className="md:basis-[60%] px-10 md:mb-10">
+            <div className=" md:basis-[40%] md:order-1 mb-3">
+              <img
+                className="pt-10 md:pt-0 w-[200px]"
+                alt="ImginifyLogo.png"
+                src={ImginifyLogo}
+              />
+            </div>
+            <p className="dark:text-[#BEE0D6] text-black text-xl">
+              Imaginify: Is a web platform that utilizes OpenAI's DALL-E API to
+              generate images based on prompts provided by the user.<br></br>In
+              addition to the AI-generated images, the app also provides a
+              library of high-definition, royalty-free images from Unsplash,
+              allowing users to browse and download images to use in their
+              projects.
+            </p>
+            <div className="flex flex-col xs:flex-row gap-2 pt-5">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://imaginify.netlify.app/"
+                className="hover:opacity-[0.8] transition duration-500 hover:scale-[1.1]"
+              >
+                <button
+                  className="p-3 w-full xs:p-3.5 dark:bg-[#00FFB9] bg-redhot dark:text-black text-white  rounded-sm py-5 px-7 font-semibold
+              hover:bg-blue hover:text-white transition duration-500"
+                >
+                  Demo website
+                </button>
+              </a>
+
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/MuhammadOsama169/Imaginify"
+                className="hover:opacity-[0.8] transition duration-500 hover:scale-[1.1]"
+              >
+                <button className="dark:bg-[#F3EFE0]  text-black bg-white w-full p-3 flex flex-row  hover:bg-blue  transition duration-500 text-center align-middle items-center justify-center">
+                  <img
+                    className="w-[30px] xs:w-[30px] xs:h-[27px] mr-2"
+                    alt="github.png"
+                    src={githubLogo}
+                  />
+                  View Code
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        {/* Superkick */}
+        <div className="flex flex-col xs:flex-col md:flex-row gap-1 pb-5 mt-10">
+          <div className="md:basis-[40%] md:order-1 basis-0 flex-col">
+            <img
+              alt="SuperKickProject.png"
+              src={superkickProject}
+              className=" md:w-auto"
+            />
+          </div>
+
+          <div className="md:basis-[60%] px-10 ">
+            <div className=" md:basis-[40%] md:order-1 mb-3 ">
+              <img
+                className="pt-10 md:pt-0"
+                alt="SuperKickLogo.png"
+                src={superkickLogo}
+              />
+            </div>
+            <p className="dark:text-[#BEE0D6] text-black text-xl ">
+              {' '}
+              SuperKick Branding designs clear brand messaging to help your
+              business stand out. For a business to stand out today the brand
+              has to be unique and impactful.
+            </p>
+
+            <div className="flex flex-col xs:flex-row gap-2 pt-5">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://superkickbranding.netlify.app/"
+                className="hover:opacity-[0.8] transition duration-500 hover:scale-[1.1]"
+              >
+                <button
+                  className="p-3 w-full xs:p-3.5 dark:bg-[#00FFB9] bg-redhot dark:text-black text-white rounded-sm py-5 px-7 font-semibold
+              hover:bg-blue hover:text-white transition duration-500"
+                >
+                  Demo website
+                </button>
+              </a>
+
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/MuhammadOsama169/project-outlaw"
+                className="hover:opacity-[0.8] transition duration-500 hover:scale-[1.1]"
+              >
+                <button className="dark:bg-[#F3EFE0] bg-white w-full p-3 flex flex-row text-black hover:bg-blue  transition duration-500 text-center align-middle items-center justify-center">
+                  <img
+                    className="w-[30px] xs:w-[30px] xs:h-[27px] mr-2"
+                    alt="github.png"
+                    src={githubLogo}
+                  />
+                  View Code
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div>
+          {/* HomeRun */}
+          <div className="flex flex-col xs:flex-col md:flex-row gap-1 pb-5 mt-10">
+            <div className=" md:basis-[40%] md:order-2 lg:order-1">
+              <img alt="HomerunProject.png" src={homerunProject} />
             </div>
 
             <div className="md:basis-[60%] px-10">
-              <div className="w-[250px] md:basis-[40%] md:order-1 mb-3"> 
-                <img  className="w-[200px] xs:w-auto pt-10 md:pt-0" alt="RenewbeLogo.png" src={renewbeLogo} />
+              <div className=" md:basis-[40%] md:order-1 mb-3">
+                <img
+                  className="pt-10 md:pt-0"
+                  alt="HomeRunLogo.png"
+                  src={homerunLogo}
+                />
               </div>
-              <p className="dark:text-[#BEE0D6] text-black text-xl"> Renewbe: A community-driven platform to support local clean energy, save money and help environmental NGOs Subscribe to use or rent a solar panel in a remote location to support clean energy in your country and save money from your electricity bill.</p> 
-          
+              <p className="dark:text-[#BEE0D6] text-black text-xl">
+                HomeRun: Is a E-commerce website with custom backend entries and
+                secure payemnt linked with STRIPE payment method.{' '}
+              </p>
               <div className="flex flex-col xs:flex-row gap-2 pt-5">
-
-                <a target="_blank" rel="noreferrer" href="https://renewbe.netlify.app/" className="hover:opacity-[0.8] transition duration-500 hover:scale-[1.1]">
-                  <button className="p-3 w-full xs:p-3.5 dark:bg-[#00FFB9] bg-redhot dark:text-black text-white  rounded-sm py-5 px-7 font-semibold
-              hover:bg-blue hover:text-white transition duration-500">Demo website</button> 
+                <a
+                  href="https://homerun-muhammad.netlify.app/"
+                  className="hover:opacity-[0.8] transition duration-500 hover:scale-[1.1]"
+                >
+                  <button
+                    className="p-3 w-full xs:p-3.5 dark:bg-[#00FFB9] bg-redhot dark:text-black text-white  rounded-sm py-5 px-7 font-semibold
+              hover:bg-blue hover:text-white transition duration-500"
+                  >
+                    Demo website
+                  </button>
                 </a>
 
-                <a target="_blank" rel="noreferrer" href="https://github.com/MuhammadOsama169/Renewbe" className="hover:opacity-[0.8] transition duration-500 hover:scale-[1.1]">
-                  <button className="dark:bg-[#F3EFE0] text-black bg-white w-full p-3 flex flex-row  hover:bg-blue transition duration-500 text-center align-middle items-center justify-center">
-                      <img className="w-[30px] xs:w-[30px] xs:h-[27px] mr-2" alt="github.png" src={githubLogo}/>
-                      View Code
-                    </button> 
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/MuhammadOsama169/HomeRun"
+                  className="hover:opacity-[0.8] transition duration-500 hover:scale-[1.1]"
+                >
+                  <button className="dark:bg-[#F3EFE0]  text-black bg-white w-full p-3 flex flex-row  hover:bg-blue  transition duration-500 text-center align-middle items-center justify-center">
+                    <img
+                      className="w-[30px] xs:w-[30px] xs:h-[27px] mr-2"
+                      alt="github.png"
+                      src={githubLogo}
+                    />
+                    View Code
+                  </button>
                 </a>
-
               </div>
             </div>
           </div>
+        </div>
+        <div>
+          {/* renewbe*/}
+          <div className="flex flex-col xs:flex-col md:flex-row gap-1 pb-5 mt-10">
+            <div className=" md:basis-[40%] md:order-2">
+              <img alt="RenewbeProject.png" src={renewbeProject} />
+            </div>
+
+            <div className="md:basis-[60%] px-10">
+              <div className="w-[250px] md:basis-[40%] md:order-1 mb-3">
+                <img
+                  className="w-[200px] xs:w-auto pt-10 md:pt-0"
+                  alt="RenewbeLogo.png"
+                  src={renewbeLogo}
+                />
+              </div>
+              <p className="dark:text-[#BEE0D6] text-black text-xl">
+                {' '}
+                Renewbe: A community-driven platform to support local clean
+                energy, save money and help environmental NGOs Subscribe to use
+                or rent a solar panel in a remote location to support clean
+                energy in your country and save money from your electricity
+                bill.
+              </p>
+
+              <div className="flex flex-col xs:flex-row gap-2 pt-5">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://renewbe.netlify.app/"
+                  className="hover:opacity-[0.8] transition duration-500 hover:scale-[1.1]"
+                >
+                  <button
+                    className="p-3 w-full xs:p-3.5 dark:bg-[#00FFB9] bg-redhot dark:text-black text-white  rounded-sm py-5 px-7 font-semibold
+              hover:bg-blue hover:text-white transition duration-500"
+                  >
+                    Demo website
+                  </button>
+                </a>
+
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/MuhammadOsama169/Renewbe"
+                  className="hover:opacity-[0.8] transition duration-500 hover:scale-[1.1]"
+                >
+                  <button className="dark:bg-[#F3EFE0] text-black bg-white w-full p-3 flex flex-row  hover:bg-blue transition duration-500 text-center align-middle items-center justify-center">
+                    <img
+                      className="w-[30px] xs:w-[30px] xs:h-[27px] mr-2"
+                      alt="github.png"
+                      src={githubLogo}
+                    />
+                    View Code
+                  </button>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-
-
+      </div>
     </section>
   );
 };
