529f238dad0b197a1e9a527da7fc039a9ffef63b
Muhammad Osama <m.osama@sustainablestar.com.sa>
2025-01-07 12:28:24 +0300

    Add google tag manager

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {App} from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { App } from "./App";
 import "./i18n";
+import TagManager from "react-gtm-module";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const tagManagerArgs = {
+  gtmId: process.env.REACT_APP_GTM_KEY,
+};
+TagManager.initialize(tagManagerArgs);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
